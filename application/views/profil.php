8296a33fc20be8c662802a9168102c5aae776635
rizal <alrizalfikri1@gmail.com>
2020-05-28 02:11:49 -0700

    da
    do

--- application/views/profil.php
+++ application/views/profil.php
@@ -170,98 +170,19 @@ include ('koneksi.php');
 							<li class="active">Data Diri</li>
 						</ul><!-- /.breadcrumb -->
 
-						<section class="content">
-							
-							<div class="page-header">
-							<h1>
-								Profil Sales
-								
-							</h1>
-						</div><!-- /.page-header -->
 
-						<div class="row">
-							<div class="col-xs-12">
-								<div>
-									<div id="user-profile-1" class="user-profile row">
-										<div class="col-xs-12 col-sm-3 center">
-											<div>
-												<span class="profile-picture">
-													<img id="avatar" class="editable img-responsive" alt="Alex's Avatar" src="<?php echo base_url()?>assets/images/avatars/profile-pic.jpg" />
-												</span>
 
-												<div class="space-4"></div>
-
-												<div class="width-80 label label-info label-xlg arrowed-in arrowed-in-right">
-													<div class="inline position-relative">
-														<a href="#" class="user-title-label dropdown-toggle" data-toggle="dropdown">
-															<i class="ace-icon fa fa-circle light-green"></i>
-															&nbsp;
-															<span class="white">Alex M. Doe</span>
-														</a>
-															</li>
-														</ul>
-													</div>
-												</div>
-											</div>
-										</div>
-												<div class="profile-info-row">
-													<div class="profile-info-name"> Username </div>
-
-													<div class="profile-info-value">
-														<span class="editable" id="username">alexdoe</span>
-													</div>
-												</div>
-
-												<div class="profile-info-row">
-													<div class="profile-info-name"> Location </div>
-
-													<div class="profile-info-value">
-														<i class="fa fa-map-marker light-orange bigger-110"></i>
-														<span class="editable" id="country">Netherlands</span>
-														<span class="editable" id="city">Amsterdam</span>
-													</div>
-												</div>
-
-												<div class="profile-info-row">
-													<div class="profile-info-name"> Age </div>
-
-													<div class="profile-info-value">
-														<span class="editable" id="age">38</span>
-													</div>
-												</div>
-
-												<div class="profile-info-row">
-													<div class="profile-info-name"> Joined </div>
-
-													<div class="profile-info-value">
-														<span class="editable" id="signup">2010/06/20</span>
-													</div>
-												</div>
-
-												<div class="profile-info-row">
-													<div class="profile-info-name"> Last Online </div>
-
-													<div class="profile-info-value">
-														<span class="editable" id="login">3 hours ago</span>
-													</div>
-												</div>
-
-												<div class="profile-info-row">
-													<div class="profile-info-name"> About Me </div>
-
-													<div class="profile-info-value">
-														<span class="editable" id="about">Editable as WYSIWYG</span>
-													</div>
-												</div>
-											</div>
-						</section>
-
-						
+			<div class="main-content-inner">
+				<section class="wrapper">
+				<span><i style="margin-left: 10px; font-size: 40px"> WELCOME ALRIZAL! </span></i>
+				</section>						
+			</div>		
 
 					
 								</div><!-- /.pull-left -->
 							</div><!-- /.ace-settings-box -->
 						</div><!-- /.ace-settings-container -->
+
 
 						<!--<div class="page-header">
 							<h1>
